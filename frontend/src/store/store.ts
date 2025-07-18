efd01e42d5a5287e46cc0a1d4d5a381d367fd177
Raul <raulalfonzo66@gmail.com>
2025-07-17 20:06:34 -0400

    Feat (store): añadir nuevos valores al store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,14 +1,37 @@
 import { create } from 'zustand'
 import {devtools} from 'zustand/middleware'
 
+type UsuarioEmpresa = {
+  id: number | null,
+  email: string,
+  nombre: string,
+  apellido: string,
+  role: string,
+  telefono: string,
+}
+
 type Store = {
   /* Empresa */
   empresa: string
   setEmpresa: (val: string) => void
+  /* Usuario Empresa */
+  usuarioEmpresa: UsuarioEmpresa
+  setUsuarioEmpresa: (usuarioEmpresa: UsuarioEmpresa) => void
 }
 
 export const useStore = create<Store>()((set) => ({
+  /* Empresa */
   empresa: '',
   setEmpresa: (empresa) => set(() => ({ empresa })),
+  /* Usuario Empresa */
+  usuarioEmpresa: {
+    id: null,
+    email: '',
+    nombre: '',
+    apellido: '',
+    role: '',
+    telefono: ''
+  },
+  setUsuarioEmpresa: (usuarioEmpresa: UsuarioEmpresa) => set(() => ({ usuarioEmpresa })),
 }))
 
